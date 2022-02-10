dcbccebb1d19004f62973dd508a77aa879351bc4
Marlon-Paulo-Da-Silva <marlon.paulo.silva@outlook.com>
2022-02-10 13:05:33 -0300

    criando o input cadastrar produtos

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,1 +1,2 @@
 export * from './Text/Index';
+export * from './Input';
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,10 @@
-import { Text } from "../components";
+import { Input, Text } from "../components";
 
 export const Home = () => {
   return (
     <div>
       <Text fontWeight="bold" >Home</Text>
+      <Input></Input>
     </div>
   );
 };
